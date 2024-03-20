15e8b7b25e77a0f5f04a5849bdd41894ca7addc2
MatheusCrivellaro <128638676+MatheusCrivellaro@users.noreply.github.com>
2024-03-19 21:36:35 -0300

    Update JogadorService

--- gerenciador-jogador/src/main/java/br/com/fiap/service/JogadorService.java
+++ gerenciador-jogador/src/main/java/br/com/fiap/service/JogadorService.java
@@ -12,11 +12,11 @@ import br.com.fiap.repository.JogadorRepository;
 public class JogadorService {
 	
 	public static void mock(JogadorRepository repository) {
-		repository.save(new Jogador(1, "Matheus", Posicao.ATACANTE, Time.ATHLETICOPR, 1.7, Dominante.DIREITO, LocalDate.now().toString()));
-		repository.save(new Jogador(2, "Gabriel", Posicao.GOLEIRO, Time.BAHIA, 1.8, Dominante.DIREITO, LocalDate.now().toString()));
-		repository.save(new Jogador(3, "Carol", Posicao.MEIA, Time.BRAGANTINO, 1.6, Dominante.ESQUERDO, LocalDate.now().toString()));
-		repository.save(new Jogador(4, "Lucas", Posicao.ZAGUEIRO, Time.CRUZEIRO, 1.7, Dominante.DIREITO, LocalDate.now().toString()));
-		repository.save(new Jogador(5, "Ana Luisa", Posicao.ATACANTE, Time.FLUMINENSE, 1.6, Dominante.ESQUERDO, LocalDate.now().toString()));
+		repository.save(new Jogador(1, "Matheus", Posicao.ATACANTE, Time.ATHLETICOPR, 177, Dominante.DIREITO, LocalDate.now().toString()));
+		repository.save(new Jogador(2, "Gabriel", Posicao.GOLEIRO, Time.BAHIA, 180, Dominante.DIREITO, LocalDate.now().toString()));
+		repository.save(new Jogador(3, "Carol", Posicao.MEIA, Time.BRAGANTINO, 165, Dominante.ESQUERDO, LocalDate.now().toString()));
+		repository.save(new Jogador(4, "Lucas", Posicao.ZAGUEIRO, Time.CRUZEIRO, 175, Dominante.DIREITO, LocalDate.now().toString()));
+		repository.save(new Jogador(5, "Ana Luisa", Posicao.ATACANTE, Time.FLUMINENSE, 170, Dominante.ESQUERDO, LocalDate.now().toString()));
 	}
 	
 	public static String dataFormatter(String data) {
